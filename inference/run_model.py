a711ab755c540b2a11fa0340e1f9766ed989a55c
Aryan Sinha <aryansinha1920@gmail.com>
2026-02-26 01:47:09 +0530

    updated Run model.py

--- inference/run_model.py
+++ inference/run_model.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, ROOT_DIR)
+
 import torch
 import argparse
 from tokenizers import Tokenizer
